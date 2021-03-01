4f3a63286fa072e23d40db1286966cc856fd3e79
hugo fuchs <hugo.haenel@live.fr>
2021-03-01 15:54:49 +0100

    js

---
+++ main.js
@@ -0,0 +1,16 @@
+const password = document.getElementById('psw')
+const password = document.getElementById('psw-repeat')
+
+form.addEventListener('submit', (e) => {
+    
+
+    if (psw.value === psw-repeat.value) {
+        alert('Mot de passe non similaire')
+        return false;
+    }
+  
+    
+})
+
+
+    
